c5eeef7e9a063731ebbb328c689e12853ff41cc2
Maicon <maicon.008almeida@gmail.com>
2025-07-15 22:22:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text);
 }
 
-//Chave do API: AIzaSyAFvmTSn8xXlTxeDj5kSz9PSnDJzBfPRL4
-
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash";
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`; 
